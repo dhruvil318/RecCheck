01c3d2c7c27ed2a1582fedd7aa3849e6f28616a1
Dhruvil Markana <dhruvilmarkana@outlook.com>
2023-02-26 14:55:19 -0500

    added sign up function to create new member

--- dbConnection.py
+++ dbConnection.py
@@ -2,15 +2,15 @@ import mysql.connector
 
 db = mysql.connector.connect(
     host="localhost",
-    username="*****",
-    password="*****",
+    username="*",
+    password="*",
     database="reccheck"
 )
 
 
 def login(user, pwd):
     cursor = db.cursor()
-    # check for sql injections
+    # need to check for sql injections here
     cursor.execute(f"SELECT * FROM members WHERE username = '{user}';")
     result = cursor.fetchone()
     if result is None:
@@ -19,4 +19,32 @@ def login(user, pwd):
     return storedpwd == pwd
 
 
-print(login("username", "password"))
+def signup(fname, lname, user, pwd, phone, email):
+    # checks if new username is unique
+    if not verifyUsername(user):
+        return "Invalid"
+
+    cursor = db.cursor()
+    # need to check for sql injections here
+    cursor.execute(
+        f"INSERT INTO members (first_name, last_name, username, password, phone, email) VALUES ('{fname}', '{lname}', '{user}', '{pwd}', '{phone}', '{email}');")
+    db.commit()
+
+    result = cursor.rowcount
+    if result == 1:
+        return "Success"
+    else:
+        return "Error"
+
+
+def verifyUsername(username):
+    cursor = db.cursor()
+    cursor.execute(
+        f"SELECT * FROM members where username = '{username}';")
+
+    result = cursor.fetchall()
+    return cursor.rowcount == 0
+
+
+signup('fname', 'lname', 'user', 'pwd',
+       'phonenumber', 'emailaddr')
