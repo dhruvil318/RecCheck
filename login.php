f3d5c44bc81251bc1e06ef134e17d28cc36a5c13
Dhruvil Markana <dhruvilmarkana@outlook.com>
2023-03-02 16:42:09 -0500

    added login backend that verifies if user is in db

--- login.php
+++ login.php
@@ -1,4 +1,27 @@
 <?php
-    $user = $_GET["username"];
-    echo "<p>$user</p>";
+$user = $_POST["username"];
+$pwd = $_POST["password"];
+$hashed_pwd = hash("sha256", "$pwd");
+
+$conn = new mysqli("reccheckdb.ctrhgcjnjceq.us-east-1.rds.amazonaws.com", "team17", "mIqmpqkB4McGexJiD7lV","reccheck");
+if(!$conn->ping()){
+    echo "Error connecting to db";
+}
+
+$result = $conn->query("SELECT * FROM members WHERE username = '$user'");
+if($result->num_rows == 1){
+    $row = $result->fetch_row();
+    $first_name = $row[1];
+    $stored_pwd = $row[4];
+    
+    if($stored_pwd == $hashed_pwd){
+        echo "Welcome $first_name";
+    }
+    else{
+        echo "Incorrect password";
+    }
+}
+else{
+    echo "Username does not exist";
+}
 ?>
