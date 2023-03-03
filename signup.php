29747d6de81cae7cdd872570fb85b23f6197e64e
Dhruvil Markana <dhruvilmarkana@outlook.com>
2023-03-03 11:02:52 -0500

    applied html change to php files

--- signup.php
+++ signup.php
@@ -1,25 +1,105 @@
-<?php
-$fname = $_POST["firstName"];
-$lname = $_POST["lastName"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
-$user = $_POST["username"];
-$pwd = $_POST["password"];
-$hashed_pwd = hash("sha256", "$pwd");
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset = "UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="index.css">
+		<link rel="stylesheet" href="signup.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+		<title>Sign Up</title>
+	</head>
 
-$conn = new mysqli("reccheckdb.ctrhgcjnjceq.us-east-1.rds.amazonaws.com", "team17", "mIqmpqkB4McGexJiD7lV","reccheck");
-if(!$conn->ping()){
-    echo "Error connecting to database";
-}
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+				<div class="navbar-brand">
+					<a class="nav-link" href="index.html">RecCheck</a>
+				</div>
 
-$duplicate_users = $conn->query("SELECT * FROM members where username = '$user';");
-if($duplicate_users->num_rows == 0){
-    $result = $conn->query("INSERT INTO members (first_name, last_name, username, password, phone, email) VALUES ('$fname', '$lname', '$user', '$hashed_pwd', '$phone', '$email')");
-    if($conn->affected_rows == 1){
-        echo "Succesfully added new user";
-    }
-}
-else{
-    echo "Username already taken";
-}
-?>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+					<span class="navbar-toggler-icon"></span>
+				</button>  
+				<div class="collapse navbar-collapse" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+						<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
+								<div class="dropdown-menu">
+									<a class="dropdown-item">Calendar</a>
+									<a class="dropdown-item">Planner</a>
+									<a class="dropdown-item">Equipment</a>
+								</div>
+						</li>
+					</ul>
+					
+					<ul class="nav navbar-nav ml-auto w-110 justify-content-end">
+						<li class="nav-item"><a class="nav-link" href="login.html">Log In</a></li>
+						<li class="nav-item"><a class="nav-link" href="signup.html">Sign Up</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+        <?php
+            $fname = $_POST["firstName"];
+            $lname = $_POST["lastName"];
+            $phone = $_POST["phone"];
+            $email = $_POST["email"];
+            $user = $_POST["username"];
+            $pwd = $_POST["password"];
+            $hashed_pwd = hash("sha256", "$pwd");
+
+            $conn = new mysqli("reccheckdb.ctrhgcjnjceq.us-east-1.rds.amazonaws.com", "team17", "mIqmpqkB4McGexJiD7lV","reccheck");
+            if(!$conn->ping()){
+                echo "Error connecting to database";
+            }
+
+            $duplicate_users = $conn->query("SELECT * FROM members where username = '$user';");
+            if($duplicate_users->num_rows == 0){
+                $result = $conn->query("INSERT INTO members (first_name, last_name, username, password, phone, email) VALUES ('$fname', '$lname', '$user', '$hashed_pwd', '$phone', '$email')");
+                if($conn->affected_rows == 1){
+                    echo "Succesfully added new user";
+                }
+            }
+            else{
+                echo "Username already taken";
+            }
+        ?>
+		<main>
+			<h1>Sign Up</h1>
+
+			<form method="post" action="signup.php">
+				<label for="username">Username</label>
+				<input type="text" name="username" id="username" required>
+				
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" required>
+				
+				<label for="firstName">First Name</label>
+				<input type="text" id="firstName" name="firstName" required>
+				
+				<label for="lastName">Last Name</label>
+				<input type="text" id="lastName" name="lastName" required>
+				
+				<label for="phone">Phone Number</label>
+				<input type="tel" id="phone" name="phone" required>
+				
+				<label for="email">Email Address</label>
+				<input type="email" id="email" name="email" required>
+				
+				<input type="submit" value="Sign Up">
+			</form>
+		</main>
+		
+		<footer class="container-fluid">
+			<div class="row">
+				<div class="col-sm">
+					<p id = "copyright">&copy; Copyright 2023 RecCheck</p>
+				</div>	
+			</div>
+		</footer>
+	</body>
+</html>
