4f4b56fb70690f102318533ed47e51146c29d633
jmanquach <jmanquach@gmail.com>
2023-03-09 15:29:09 -0500

    Update javatext.js

--- javatext.js
+++ javatext.js
@@ -1,7 +1,7 @@
 function sendEmail() {
   Email.send({
     SecureToken : "dd3121de-ce79-47d9-924d-e9e0022cf6a2",
-    To : 'jmanquachcoder@gmail.com',
+    To : 'kwongchong8888@gmail.com',
     From : "jmanquachcoder@gmail.com",
     Subject : "RecCheck Alert",
     Body : "Customer Needs Help At Front Desk!"
